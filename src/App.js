910d72e588829f96f20cc1fafb30b32af4334728
Mauricio tertuliano Santos Silva <104770045+Mauricio-tertu@users.noreply.github.com>
2022-11-16 15:12:26 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {FiSearch} from 'react-icons/fi';
+import './style.css'
+
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+   async function handleSearch(){
+    
+
+    if(input ==='') {
+      alert ("Por favor Digitar CEP!")
+      return;
+    }
+try { 
+const response = await api.get('${input}/json');
+setCep(response.data);
+setInput ("");
+
+
+}catch{
+alert("ops não reconhecemos esse cep");
+setInput ("");
+}
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container">
+      <h1 className='title'>Consulta CEP </h1>
+      <h5 className='re'>Com React</h5>
+      <div className='containerInput'>
+        <input 
+        type="text"
+        placeholder='Digite o seu cep aqui... '
+        value={input}
+        onChange={(e) => setInput(e.target.value) }
+        />
+
+        <butoom className="buttonSearch" onClick={handleSearch} >
+          <FiSearch size={35} color="#000"/>
+        </butoom>
+      </div>
+
+     
+      {Object.keys(cep).length > 0 && (
+ <main className='main' >
+ <h2> CEP: {cep.cep} </h2>
+
+ <span> {cep.logradouro} </span>
+ <span>Complemento: {cep.complemento}  </span>
+ <span> {cep.bairro} </span>
+ <span> {cep.localidade} - {cep.uf} </span>
+</main>
+ )}
+     </div>
+ );
 }
 
 export default App;
